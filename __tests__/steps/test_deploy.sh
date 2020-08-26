74bb7267f6711ff9b5e367699583b2710446b542
Mario Lubenka <mario.lubenka@googlemail.com>
2020-08-26 18:25:18 +0200

    test: symlink current role

--- __tests__/steps/test_deploy.sh
+++ __tests__/steps/test_deploy.sh
@@ -6,9 +6,6 @@
 # This test deploys a container project onto the target server
 # IMPORTANT: This must run after test_provision.sh!
 
-# map webserver name to role name
-if [ "$WEBSERVER" == 'caddy' ]; then WEBSERVER='getstackhead.stackhead_webserver_caddy'; fi
-
 sed -e "s/\${ipaddress}/${IP}/" -e "s/\${webserver}/${WEBSERVER}/" -e "s/\${application}/container/" __tests__/inventory.dist.yml > __tests__/inventory.yml
 sed -e "s/\${domain}/${DOMAIN}/" __tests__/projects/container.dist.yml > __tests__/projects/container.yml
 TEST=1 ansible-playbook stackhead-repo/ansible/application-deploy.yml -i "${INVENTORY_PATH}" -vv
--- __tests__/steps/test_provision.sh
+++ __tests__/steps/test_provision.sh
@@ -3,9 +3,6 @@
 # Domain in environment "DOMAIN"
 INVENTORY_PATH=__tests__/inventory.yml
 
-# map webserver name to role name
-if [ "$WEBSERVER" == 'caddy' ]; then WEBSERVER='getstackhead.stackhead_webserver_caddy'; fi
-
 sed -e "s/\${ipaddress}/${IP}/" -e "s/\${webserver}/${WEBSERVER}/" __tests__/inventory.dist.yml > $INVENTORY_PATH
 
 # Install dependencies
@@ -13,8 +10,8 @@ pip install -r requirements/pip.txt
 ansible-galaxy install -r stackhead-repo/ansible/requirements/requirements.yml --force-with-deps
 
 # Remove this role and set symlink
-rm -rf ~/.ansible/roles/getstackhead.stackhead_webserver_caddy
-ln -s "$(pwd)" ~/.ansible/roles/getstackhead.stackhead_webserver_caddy
+rm -rf "${HOME}/.ansible/roles/${ROLE_NAME}"
+ln -s "$(pwd)" "${HOME}/.ansible/roles/${ROLE_NAME}"
 
 # Provision server
 TEST=1 ansible-playbook stackhead-repo/ansible/server-provision.yml -i "${INVENTORY_PATH}" -vv
