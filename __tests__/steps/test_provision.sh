8211f26ce0c0ac51b3b5c6dc430d81671fda4fb6
Mario Lubenka <mario.lubenka@googlemail.com>
2020-08-26 18:23:08 +0200

    test: symlink current role

--- __tests__/steps/test_provision.sh
+++ __tests__/steps/test_provision.sh
@@ -10,7 +10,11 @@ sed -e "s/\${ipaddress}/${IP}/" -e "s/\${webserver}/${WEBSERVER}/" __tests__/inv
 
 # Install dependencies
 pip install -r requirements/pip.txt
-ansible-galaxy install -r stackhead-repo/ansible/requirements/requirements.yml
+ansible-galaxy install -r stackhead-repo/ansible/requirements/requirements.yml --force-with-deps
+
+# Remove this role and set symlink
+rm -rf ~/.ansible/roles/getstackhead.stackhead_webserver_caddy
+ln -s "$(pwd)" ~/.ansible/roles/getstackhead.stackhead_webserver_caddy
 
 # Provision server
 TEST=1 ansible-playbook stackhead-repo/ansible/server-provision.yml -i "${INVENTORY_PATH}" -vv
