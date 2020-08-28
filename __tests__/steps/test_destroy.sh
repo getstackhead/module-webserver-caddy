ac47c2866a93ea9114a034970ff89dca645cea4b
Mario Lubenka <mario.lubenka@googlemail.com>
2020-08-28 18:54:03 +0200

    chore: use getstackhead/stackhead action (#2)
    * style: use getstackhead/stackhead action
    
    * ci: use master branch of action
    
    * tests: use variables from main repository
    
    * style: set webserver and role from env

--- __tests__/steps/test_destroy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# IP address in environment "IP"
-# Domain in environment "DOMAIN"
-# Ansible inventory path in environment "INVENTORY_PATH"
-
-# This test destroys a deployed project on the target server
-# IMPORTANT: This must run after test_deploy.sh!
-
-TEST=1 ansible-playbook stackhead-repo/ansible/application-destroy.yml -i "${INVENTORY_PATH}" --extra-vars "project_name=container" -vv
